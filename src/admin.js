11c781fd158717b07eda4f63470968646ec6b730
VaheGrigorian <v_grigorian@hotmail.com>
2020-07-21 22:04:25 +0400

    admin page

--- src/admin.js
+++ src/admin.js
@@ -1,12 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { BrowserRouter as Router} from 'react-router-dom'
-import { ThemeProvider } from 'styled-components'
-import Admin from './apps/Admin'
+import React from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter as Router } from "react-router-dom";
+import Admin from "./apps/Admin";
 
 ReactDOM.render(
-  <ThemeProvider theme={{mode: 'admin'}}>
-    <Router>
-      <Admin />
-    </Router>
-  </ThemeProvider>, document.getElementById('app'))
+  <Router>
+    <Admin />
+  </Router>,
+  document.getElementById("app")
+);
